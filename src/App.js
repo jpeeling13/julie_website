e5ae644db356c71342353b4eb5da7b8e349d48b1
Joshua Peeling <jpeeling13@outlook.com>
2019-06-29 14:16:35 -0400

    added components for pages

--- src/App.js
+++ src/App.js
@@ -2,20 +2,15 @@ import React from 'react';
 import './App.css';
 import {HashRouter as Router, Route} from "react-router-dom";
 import TopNav from "./components/TopNav.js";
+import AboutPage from "./components/AboutPage.js";
+import TestimonialsPage from "./components/TestimonialsPage.js";
 
 function App() {
   return (
     <Router basename="/">
       <Route path="/" component={TopNav} />
-      <div className="App-body">
-        <div className="splashContainer">
-          <div className="splashImage splashHandshake" />
-          <div className="splashText">
-            <h1>Julie East HR</h1>
-            <p>Coming Soon</p>
-          </div>
-        </div>
-      </div>
+      <Route path="/" exact component={AboutPage} />
+      <Route path="/testimonials" exact component={TestimonialsPage} />
     </Router>
   );
 }
